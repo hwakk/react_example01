3acb5f2adb4b510a459d5efa448f4805cd4c1bcf
hwakk <napia1014@gmail.com>
2022-02-02 23:18:52 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Movie from "./components/Movie";
 
 function App() {
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieYear, setMovieYear] = useState("");
+  const [movies, setMovies] = useState([
+    { title: "kossie coder1", year: "2001" },
+    { title: "kossie coder2", year: "2002" },
+    { title: "kossie coder3", year: "2003" },
+    { title: "kossie coder4", year: "2004" },
+  ]);
+
+  useEffect(() => {
+    console.log("render");
+  });
+
+  const renderMovies = movies.map((movie) => {
+    return <Movie movie={movie} key={movie.title} />;
+  });
+
+  const addMovie = (e: any) => {
+    e.preventDefault();
+    setMovies([
+      ...movies,
+      {
+        title: movieTitle,
+        year: movieYear,
+      },
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie List</h1>
+      <form onSubmit={addMovie}>
+        <input
+          type="text"
+          value={movieTitle}
+          placeholder="영화제목"
+          onChange={(e) => setMovieTitle(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={movieYear}
+          placeholder="개봉년도"
+          onChange={(e) => setMovieYear(e.target.value)}
+        />
+        <br />
+        <button type="submit">영화추가</button>
+      </form>
+      {renderMovies}
     </div>
   );
 }
